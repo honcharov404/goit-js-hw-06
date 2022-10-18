4b20e7a628a7616b562f0b33ac4af7f00d264b0d
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-18 11:43:22 +0300

    task 2 complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('#ingredients');
+console.log(list);
+
+ingredients.forEach(function (ingredient) {
+  const ingredientsItem = document.createElement('li');
+  ingredientsItem.textContent = ingredient;
+  ingredientsItem.classList.add('item');
+  list.after(ingredientsItem);
+});
