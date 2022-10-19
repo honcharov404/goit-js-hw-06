86a1e082c7a8a376d0acf7a011706ac7adb60cf8
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-19 14:31:13 +0300

    task 1, 3 complete

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const itemsEl = document.querySelectorAll('.item');
+
+console.log(`Number of categories: ${itemsEl.length}`);
+
+itemsEl.forEach(item => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryMarkup = images
+  .map(image => `<li><img src=${image.url} alt=${image.alt}></li>`)
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkupEl);
