20571b9fbbdf8762318741730024396a71bf48e6
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-21 14:19:55 +0300

    task-8_complete

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+  if (email.value === '' || password.value === '') {
+    return alert('Необходимо заполнить все поля');
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+
+  e.currentTarget.reset();
+}
