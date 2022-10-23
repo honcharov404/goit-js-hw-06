f956d3c489bf6dd4f3f14df98df8e7eb30a6e89c
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-23 23:43:50 +0300

    hw complete

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const nameLabel = document.querySelector('#name-output');
 inputEl.addEventListener('input', onInputChange);
 
 function onInputChange(e) {
-  if (e.currentTarget.value > 0) {
+  if (e.currentTarget.value !== '') {
     nameLabel.textContent = e.currentTarget.value;
   } else {
     nameLabel.textContent = 'Anonymous';
