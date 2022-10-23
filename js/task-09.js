4c1c39ed261ff35db43f4d93170d83fd9ac3746c
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-10-23 15:16:42 +0300

    task-9_edit_complete

--- js/task-09.js
+++ js/task-09.js
@@ -5,18 +5,11 @@ const spanEl = document.querySelector('.color');
 changeColorBtn.addEventListener('click', onChangeBgc);
 
 function onChangeBgc(e) {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  spanEl.textContent = getRandomHexColor();
+  let color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
-// console.log(getRandomHexColor());
-
-// 1 создать переменные для кнопки и спана
-
-// 2 повесить слушателя на кнопку по событию клик
-
-// 3 написать функцию для присвоения стиля бэкграунда и присвоению значения для спана
